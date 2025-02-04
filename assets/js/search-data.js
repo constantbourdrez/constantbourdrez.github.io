67a59f3fb78cb37c0159d4a16a33016882bf2670
constantbourdrez <113033706+constantbourdrez@users.noreply.github.com>
2025-02-04 13:09:06 +0000

    Deploying to gh-pages from @ constantbourdrez/constantbourdrez.github.io@f103a4a1b48299a74bd95f90b06dbac948e913bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -520,7 +520,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://soundcloud.com/gali-constant", "_blank");
         },
       },{
       id: 'light-theme',
